341c45c84a97e9e055c3355e08be8ad905f2016f
moose <moosecodes@gmail.com>
2023-02-12 11:15:08 -0800

    add clear button and error message

--- resources/js/Components/ArticleContent.vue
+++ resources/js/Components/ArticleContent.vue
@@ -10,12 +10,28 @@ const props = defineProps({
 onMounted(() => {
     searchResult.value = [...props.news]
 })
-const searchQuery = ref('')
+let searchQuery = ref('')
 const searchResult = ref([])
-const searchNews = (searchQuery) => {
-    axios.post('/api/news/search', { searchQuery }).then(res => {
-        searchResult.value = [...res.data]
+const searchNews = (query) => {
+    axios.post('/api/news/search', { searchQuery: query }).then(res => {
+        if(!res.data.length) {
+            searchResult.value = [{
+                "id":19,"source":"Not Found","author":"Please try searching again",
+                "title":"Your search did not find anything",
+                "description":"Leading theories suggest that the first energy used by life was either from the sun or from geothermal heat and chemistry at the bottom of the ocean.",
+                "url":"https:\/\/www.moosecodes.com\/404",
+                "urlToImage":"https:\/\/cdn.mos.cms.futurecdn.net\/m5LRyF95WHb5AYRfZswX7b-1200-80.jpg",
+            }]
+        } else {
+            searchResult.value = [...res.data]
+            console.log(searchResult.value)
+        }
     })
+}
+
+const clear = () => {
+    searchQuery.value = ''
+    searchNews()
 }
 
 const likeArticle = (id) => {
@@ -34,7 +50,8 @@ const likeArticle = (id) => {
 <template>
     <div class="flex justify-end font-semibold text-xl text-gray-600 leading-tight mb-8">
         <TextInput type="text" @keydown.enter="searchNews(searchQuery)" v-model="searchQuery" />
-        <PrimaryButton class="ml-2" @click="searchNews(searchQuery)">Search</PrimaryButton>
+        <PrimaryButton class="ml-2" @click="searchNews(searchQuery)" :disabled="!searchQuery.length">Search</PrimaryButton>
+        <PrimaryButton class="ml-2" @click="clear()" :disabled="!searchQuery.length">Clear</PrimaryButton>
     </div>
     <div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 text-xl text-gray-700 mb-8">
         <div v-for="(article, i) in searchResult" :key="i" class="hover:scale-105">
